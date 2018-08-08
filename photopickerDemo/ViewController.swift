20c5dfef7253f0230ce0ab15806b6fb6681a03b4
saurabh-vijay004 <saurabh.vijay@iksulaops.com>
2018-08-08 14:33:55 +0530

    Gallery Photo Picker

--- photopickerDemo/ViewController.swift
+++ photopickerDemo/ViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageview: UIImageView!
+    
+    
+    @IBAction func importImage(_ sender: Any) {
+    
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            
+            imageview.image = image
+            self.dismiss(animated: true, completion: nil)
+        } else{
+            
+            print("There was some issue picking image");
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
